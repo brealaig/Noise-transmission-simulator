8b5ae0132d612b1ac6c12a19a34d9bef38ad4fe6
Gabriela Martínez Eslava <gabrielaeslava22m@gmail.com>
2025-03-09 22:03:56 -0500

    Delete EdificioE directory

--- EdificioE/src/Modelo/ElementosEdificio/FuenteInterna.java
+++
@@ -1,8 +0,0 @@
-package Modelo.ElementosEdificio;
-
-public class FuenteInterna extends Fuente{
-
-    public FuenteInterna(String identificador, int frecuencia, int duracion, String tipo){
-        super(identificador,frecuencia,duracion,tipo);
-    }  
-}
